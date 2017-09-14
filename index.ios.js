89a8867c684354eea0f693a91590ecf9752f9d9e
joebelmonte <joseph.belmonte@gmail.com>
2017-09-14 16:20:54 -0400

    Some text showing up on screen

--- index.ios.js
+++ index.ios.js
@@ -1,44 +1,11 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+// Import a library to help create a component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
 
-import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View } from 'react-native';
+// Create a Component
 
-export default class albums extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit index.ios.js</Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
-    );
-  }
-}
+const App = () => <Text> Some text</Text>;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF'
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5
-  }
-});
+// Render it to the device
 
-AppRegistry.registerComponent('albums', () => albums);
+AppRegistry.registerComponent('albums', () => App);
